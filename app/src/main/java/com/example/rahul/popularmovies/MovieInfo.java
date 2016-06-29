d75385e8c84c0b6736a84219d2592a0ccb27b351
Rahul Ramkumar <rahulram226@gmail.com>
2016-06-29 18:09:36 -0400

    Add Required Data to Class

--- app/src/main/java/com/example/rahul/popularmovies/MovieInfo.java
+++ app/src/main/java/com/example/rahul/popularmovies/MovieInfo.java
@@ -1,15 +1,23 @@
 package com.example.rahul.popularmovies;
 
+import java.util.Date;
+
 /**
  * Created by rahul on 6/25/16.
  */
 public class MovieInfo {
     private String name;
     private String pictureUrl;
+    private String overview;
+    private float rating;
+    private Date release;
 
-    MovieInfo(String name, String pictureUrl) {
+    public MovieInfo(String name, String pictureUrl, String overview, float rating, Date release) {
         this.name = name;
         this.pictureUrl = pictureUrl;
+        this.overview = overview;
+        this.rating = rating;
+        this.release = release;
     }
 
     public String getName() {
@@ -18,5 +26,17 @@ public class MovieInfo {
 
     public String getPictureUrl() {
         return pictureUrl;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public Date getRelease() {
+        return release;
     }
 }
